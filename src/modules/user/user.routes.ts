0ab4888c75a94901e617984cf39baab2f30c5d48
GabrielSiv <gabriel.siv720@gmail.com>
2026-06-08 21:21:47 -0300

    feat: create find user

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,3 +1,10 @@
 import type { FastifyInstance } from 'fastify';
 import { UserController } from './user.controller.js';
 import { checkApiKey } from '../../infra/middlewares/auth.js';
+
+export async function userRoutes(app: FastifyInstance) {
+  const userController = new UserController();
+  app.addHook('preHandler', checkApiKey);
+
+  app.post('/create_find_user', userController.handleCreate.bind(userController));
+}
